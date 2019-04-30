dc3ef3eae73c5b9f02696f8b24a5456cf6accce7
Ryan David Forsyth <forsyth.r@gmail.com>
2019-04-30 15:48:15 -0400

    Finish fundamentals problem 2-3

--- exercises/1_fundamental/2_banking.js
+++ exercises/1_fundamental/2_banking.js
@@ -1,33 +1,31 @@
 class Store {
-    constructor(red, initialState) {
-        this.reducer = red
-        this.state = initialState
-    }
-    dispatch(dispatchedAction) {
-        this.state = this.reducer(this.state, dispatchedAction)
-    }
+   constructor(red, initialState) {
+      this.reducer = red
+      this.state = initialState
+   }
+   dispatch(dispatchedAction) {
+      this.state = this.reducer(this.state, dispatchedAction)
+   }
 }
 
 let reducer = function (state, action) {
-    if (action.type === "withdraw") {
-        return state - action.content
-    }
-    if (action.type === "deposit") {
-        return state + action.content
-    }
-    return state
+   if (action.type === "withdraw") {
+      return state - action.content
+   }
+   if (action.type === "deposit") {
+      return state + action.content
+   }
+   return state
 }
 
 let store = new Store(reducer, 100)
 
-store.dispatch({ type: "deposit", content: 30 })
-
 // Insert your code after this line
-
+store.dispatch({ type: "withdraw", content: 50 })
 // Insert your code before this line 
 
 if (store.state !== 50) {
-    throw new Error("expected a balance of 50 but got " + store.state)
+   throw new Error("expected a balance of 50 but got " + store.state)
 }
 
 console.log("Exercise finished")
